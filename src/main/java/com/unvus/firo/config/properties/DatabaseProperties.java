25a7e6552589feb329e43c28c3a449fd9df12695
guavatak <guava.tak@gmail.com>
2021-02-17 19:31:16 +0900

    개발서버 Firo 설정 변경

--- src/main/java/com/unvus/firo/config/properties/DatabaseProperties.java
+++ src/main/java/com/unvus/firo/config/properties/DatabaseProperties.java
@@ -9,15 +9,15 @@ import org.springframework.stereotype.Component;
 @Setter
 @Component("firoDatabaseProperties")
 public class DatabaseProperties {
-    @Value("${firo.database.driver-class-name:com.zaxxer.hikari.HikariDataSource}")
+    @Value("${unvus.firo.database.driver-class-name:com.zaxxer.hikari.HikariDataSource}")
     private String driverClassName;
 
-    @Value("${firo.database.url:jdbc:h2:file:~/.firo/h2db/db/firo;DB_CLOSE_DELAY=-1}")
+    @Value("${unvus.firo.database.url:jdbc:h2:file:~/.firo/h2db/db/firo;DB_CLOSE_DELAY=-1}")
     private String url;
 
-    @Value("${firo.database.username:firouser}")
+    @Value("${unvus.firo.database.username:firouser}")
     private String username;
 
-    @Value("${firo.database.password:#{null}}")
+    @Value("${unvus.firo.database.password:#{null}}")
     private String password;
 }
