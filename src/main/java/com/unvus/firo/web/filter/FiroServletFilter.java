4da9c7878c10f2c403a53ef50798a9fe52faab20
guavatak <guava.tak@gmail.com>
2022-07-20 15:35:08 +0900

    firo servlet filter 자동 설정 제거

--- src/main/java/com/unvus/firo/web/filter/FiroServletFilter.java
+++ src/main/java/com/unvus/firo/web/filter/FiroServletFilter.java
@@ -1,20 +1,15 @@
 package com.unvus.firo.web.filter;
 
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Order(value = Ordered.HIGHEST_PRECEDENCE)
-@WebFilter(filterName = "firoServletFilter", urlPatterns = "/*")
 public class FiroServletFilter extends OncePerRequestFilter {
 
     @Override
